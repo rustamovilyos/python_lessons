7800dde78e2384cd9612c656144d880623b4e9f4
rustamovilyos <rustamov7871@gmail.com>
2021-03-29 02:15:50 +0500

    O'zimni ilk o'yinim bo'yicha: award(yutuq) umumiy natijani chiqaruvchi o'zgaruvchi qo'shdim va koddagi bazi o'zgarishlar

--- py_lessons_for_github/my_game/qo'shish.py
+++ py_lessons_for_github/my_game/qo'shish.py
@@ -1,35 +1,38 @@
 
 import random
 import time
-
+award = 0
+award_time = 0
 jon = 3
 while True:
     boshlangan_vaqt = time.time()
-    amallar = ['+', '-']
     birinchi_son = random.randint(1,3)
     ikkinchi_son = random.randint(1,4)
     uchinchi_son = random.randint(1,3)
-    print('\n Boshlangan vaqt: ', int(boshlangan_vaqt - time.time()))
     print(birinchi_son, ' + ', ikkinchi_son, ' - ', uchinchi_son, ' =  ')
 
     if ((int(input('Natija = ')) == (birinchi_son + ikkinchi_son - uchinchi_son))) and (jon > 0):
+        award = award + 1
         print("✔")
         print(f'Sarflangan vaqt: {int(time.time() - boshlangan_vaqt)} s')
-        if (int(time.time() - boshlangan_vaqt)) > 4:
+        if (int(time.time() - boshlangan_vaqt)) > 5:
             jon -= 1
             print(f'{jon} ta ❤ qoldi! ')
             if jon == 0:
                 print('#TUGADI )')
+                print(f"Siz {award} ball to'pladingiz")
                 break
     else:
         print('❌')
-        if ((int(input("Yana bir marta urinib ko'ring: "))) ==
-            (birinchi_son + ikkinchi_son - uchinchi_son)) and (jon > 0):
+        ask_again = (int(input("Yana bir marta urinib ko'ring: ")))
+        if (ask_again == (birinchi_son + ikkinchi_son - uchinchi_son)) and (jon > 0):
+            award = award + 1
             print("✔")
             print(f'Sarflangan vaqt: {int(time.time() - boshlangan_vaqt)} s')
-            if (int(time.time() - boshlangan_vaqt)) > 4:
+            if (int(time.time() - boshlangan_vaqt)) > 5:
                 jon -=1
                 print(f'{jon}ta ❤ qoldi!')
                 if jon == 0:
                     print('#TUGADI )')
+                    print(f"Siz {award} ball to'pladingiz")
                     break
