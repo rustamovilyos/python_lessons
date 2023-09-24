d7963e1caaca2b1b2bfec66336870e9cb434d863
Ilyos Rustamov <81313741+rustamovilyos@users.noreply.github.com>
2023-09-24 14:56:08 +0500

    Update rome_to_arabic_numbers.py

--- py_lessons_for_github/dars_14/rome_to_arabic_numbers.py
+++ py_lessons_for_github/dars_14/rome_to_arabic_numbers.py
@@ -6,14 +6,19 @@
 # C = 100
 # D = 500
 # M = 1000
-def int_to_arabic(input):
-    ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
-    nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
-    result = ""
-    for i in range(len(ints)):
-        count = (input / nums[i])
-        result += nums[i] * count
-        input -= ints[i] * count
-    return result
-print(int_to_arabic(IX))
+def roman_to_arabic_num(roman_num):
+    roman_num = roman_num.upper()
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+
+    arabic_num = 0
+    for i in range(len(roman_num)):
+        if i > 0 and roman_dict[roman_num[i]] > roman_dict[roman_num[i - 1]]:
+            arabic_num += roman_dict[roman_num[i]] - 2 * roman_dict[roman_num[i - 1]]
+        else:
+            arabic_num += roman_dict[roman_num[i]]
+    return arabic_num
+
+
+print(roman_to_arabic_num('MCMXCIV'))
 
